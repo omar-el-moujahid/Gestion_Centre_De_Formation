a4390cfff4777866e56f5d8414e71f483ecc81e9
vamyusuf <ibnezzynyoussef@gmail.com>
2024-12-31 20:17:41 +0100

    youssef front

---
+++ Partie_Consumation_API_Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Partie_Consumation_API_Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
